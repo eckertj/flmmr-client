e18991f07004c6b0f2aebc129e97144c2b6f750e
Jacob Eckert <jacob.eckert@me.com>
2016-06-01 23:58:33 +0200

    added mostly markup and styling

---
+++ README.md
@@ -0,0 +1,23 @@
+# flmmr-api
+
+flmmr-client ist eine Angular 2 App, die über mit der [```flmmr-api```](https://github.com/eckertj/flmmr-api) kommuniziert um die Mediatheken der deutschen öffentlich-rechtlichen Rundfunkanstalten zu durchsuchen und Beiträge anzusehen.
+
+
+### Getting up and running
+
+```
+$ git clone git@github.com:eckertj/flmmr-client.git
+$ npm install
+$ gulp build # to build the app in dist folder
+$ gulp serve # to build and start a local browsersync
+```
+
+### Usage
+Just search for some content and watch it directly from the .mp4 source.
+
+### Try it
+
+Try it live at [```https://eckertj.github.io/flmmr-client/```](https://eckertj.github.io/flmmr-client/)!
+
+### Credits
+
--- app/components/media.component.ts
+++ app/components/media.component.ts
@@ -35,4 +35,30 @@ export class MediaComponent {
     this.mediaURLChanged.emit(media_url);
   }
 
+  focusIn() {
+    console.log("focus in");
+    $("div.search-field").removeClass("start");
+  }
+
+  focusOut() {
+    console.log("focus out");
+    $("div.search-field").addClass("start");
+  }
+
+  dateToString(datestr) {
+    var date = new Date(datestr);
+    return date.getDate() + "." + date.getMonth() + "." + date.getFullYear();
+  }
+
+  durationToString(durstr) {
+
+    var allMinutes = Math.round(durstr / 60);
+    var hours = Math.floor(allMinutes / 60);
+    var leftMinutes = allMinutes % 60;
+
+    var additionalZero = leftMinutes.toString().length == 1 ? "0" : ""
+
+    return hours + ":" + additionalZero +leftMinutes + " h";
+  }
+
 }
